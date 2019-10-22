f9979862592adbdba034c664ce7de00a077e414d
693806579@qq.com <693806579@qq.com>
2019-10-22 15:13:46 +0800

    ss

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 const App = (props:any) =>{
   const [name,setName] =useState('hello world')
+  useEffect(()=>{
+    setName('ohahahah')
+  },[])
   return (
     <div>{name}</div>
   )
